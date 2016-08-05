ed442bc7a39d0dedfd467e705f76bd881623c875
zilong.zhou <zzl81cn@163.com>
2016-08-05 18:00:27 +0800

    1.Add demo06min

--- demo06mine/main.jsx
+++ demo06mine/main.jsx
@@ -5,3 +5,9 @@ var React = require('react'),
     ReactDOM = require('react-dom'),
     Style = require('style-loader');
 
+ReactDOM.render(
+    <div>
+        <h1 className={style.h1}>HelloWorld</h1>
+        <h2 className="h2">HelloWorld</h2>
+    </div>
+)
